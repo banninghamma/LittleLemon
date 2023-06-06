c651effe1e60b864221ed706221ab3f6b9c21e4e
banninghamma <rocky_iwata@live.ca>
2023-06-05 20:13:59 -0400

    Booking table API added.

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
     path('', views.index, name='index'),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
+from rest_framework import generics, viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+
 # Create your views here.
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
 def index(request):
     return render(request, 'index.html', {})
