3c4427c2a7b077533c818742a6843ad07a769684
banninghamma <rocky_iwata@live.ca>
2023-07-17 18:46:32 -0400

    Models added

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    booking_date = models.DateField()
+    no_of_guests = models.IntegerField(null=False)
+    booking_slot = models.SmallIntegerField(default=10)
+
+    def __str__(self):
+        return self.first_name
+
+class Menu(models.Model):
+   name = models.CharField(max_length=200)
+   price = models.DecimalField(max_digits=10, decimal_places=2)
+   inventory = models.IntegerField(null=False)
+   menu_item_description = models.TextField(max_length=1000, default='') 
+
+   def __str__(self):
+      return self.name
