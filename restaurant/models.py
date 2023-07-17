a06201010c2af671fc55cd4c631959b6709a135a
banninghamma <rocky_iwata@live.ca>
2023-07-17 19:31:34 -0400

    Django REST Framework added

--- restaurant/models.py
+++ restaurant/models.py
@@ -14,7 +14,7 @@ class Menu(models.Model):
    name = models.CharField(max_length=200)
    price = models.DecimalField(max_digits=10, decimal_places=2)
    inventory = models.IntegerField(null=False)
-   menu_item_description = models.TextField(max_length=1000, default='') 
+   item_description = models.TextField(max_length=1000, default='') 
 
    def __str__(self):
       return self.name
